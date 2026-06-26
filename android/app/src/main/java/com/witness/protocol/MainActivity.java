360ba03cf91dda257bfcd52c25ee95b5460d1536
samuel1982tiem-glitch <samuel1982tiem@gmail.com>
2026-06-26 15:35:09 -0300

    Register VoiceRecorder plugin in MainActivity

--- android/app/src/main/java/com/witness/protocol/MainActivity.java
+++ android/app/src/main/java/com/witness/protocol/MainActivity.java
@@ -2,4 +2,12 @@ package com.witness.protocol;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Register the native plugin so Capacitor can find it
+        registerPlugin(VoiceRecorderPlugin.class);
+    }
+}
