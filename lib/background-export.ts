19b036891d07bd7047462218993bbd013c0e384b
samuel1982tiem-glitch <samuel1982tiem@gmail.com>
2026-07-13 16:25:48 -0300

    docs: add GPL-3.0 SPDX license headers to all source files for F-Droid compliance

--- lib/background-export.ts
+++ lib/background-export.ts
@@ -1,3 +1,21 @@
+// Witness Protocol
+// Copyright (C) 2026 Samuel Matias Tiem
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // Thin, safe wrapper around the native BackgroundExport plugin (foreground
 // service + progress notification). Every function is a no-op that never
 // throws if the plugin isn't available or misbehaves -- a notification
